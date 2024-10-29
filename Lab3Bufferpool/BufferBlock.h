1cb8892a1309f2ca6dcc7d1f4049baff746af158
Justin <jccooper7@liberty.edu>
2024-10-29 11:23:34 -0400

    Created BufferBlock.h and the functions for it

--- Lab3Bufferpool/BufferBlock.h
+++ Lab3Bufferpool/BufferBlock.h
@@ -1,1 +1,59 @@
 #pragma once
+
+#include "BufferBlockADT.h"
+#include <cstring>
+
+class BufferBlock : public BufferBlockADT
+{
+private:
+	int blockID;
+	char* block;
+	int blockSize;
+
+public:
+	BufferBlock(int blockSize = 4096) : blockSize(blockSize)
+	{
+		block = new char[blockSize];
+		blockID = -1;
+	}
+	~BufferBlock()
+	{
+		delete[] block;
+	}
+
+    //read the block from pos to pos + sz-1 (or to the end of the block)
+	void getData(int pos, int sz, char* data) override
+	{
+		memcpy(data, block + pos, sz);
+	}
+
+    //setID
+	void setID(int id) override
+	{
+		blockID = id;
+	}
+
+    //getID
+	int getID() const override
+	{
+		return blockID;
+	}
+
+    //getBlocksize
+	int getBlockSize() const override
+	{
+		return blockSize;
+	}
+
+    //return the block
+	char* getBlock() const override
+	{
+		return block;
+	}
+
+    //set the block
+	void setBlock(char* blk) override
+	{
+		memcpy(block, blk, blockSize);
+	}
+};
